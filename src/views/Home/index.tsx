635cf62a633f40575431a25f7e1a24c6548289d0
Artemw001 <artem.chumanov@artigaming.net>
2026-03-10 01:48:43 +0200

    make changes

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 import Benefits from "./Benefits";
 import Hero from "./Hero";
-import Statistic from "./Statistic";
+// import Statistic from "./Statistic";
 
 const Home = () => {
   return (
     <div className="max-w-[1980px] mx-auto px-20">
       <Hero />
       <Benefits />
-      <Statistic />
+      {/* <Statistic /> */}
     </div>
   );
 };
